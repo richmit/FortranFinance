4ec9503cf96e74ad347e80da69004dc857c73cfa
Mitch Richling <11151403+richmit@users.noreply.github.com>
2025-01-19 14:05:50 -0600

    Added header

--- monte_carlo/blend_risk_cvuc.R
+++ monte_carlo/blend_risk_cvuc.R
@@ -1,13 +1,47 @@
-
-## I ran blend_risk twice -- once correlated and once uncorrelated.  I
-## put the results in blend_risk_cor.txt & blend_risk_uncor.txt.  Then
-## I stuck them together like this:
+#!/usr/bin/env -S Rscript
+# -*- Mode:ess-r; Coding:us-ascii-unix; fill-column:158 -*-
+#########################################################################################################################################################.H.S.##
 ##
-## echo '     trial        hp             balance  Method' > blend_risk.txt
-##
-## grep -v trial blend_risk_cor.txt | sed 's/$/   Correlated/'   >> blend_risk.txt
-##
-## grep -v trial blend_risk_uncor.txt | sed 's/$/  Uncorrelated/' >> blend_risk.txt
+# @file      blend_risk_cvuc.R
+# @author    Mitch Richling http://www.mitchr.me/
+# @date      2025-01-19
+# @brief     Difference between correlated and uncorrelated MC.@EOL
+# @keywords  finance fortran monte carlo inflation cashflow time value of money tvm percentages taxes stock market
+# @std       GNU-R
+# @see       https://github.com/richmit/FortranFinance
+# @copyright 
+#  @parblock
+#  Copyright (c) 2025, Mitchell Jay Richling <http://www.mitchr.me/> All rights reserved.
+#  
+#  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#  
+#  1. Redistributions of source code must retain the above copyright notice, this list of conditions, and the following disclaimer.
+#  
+#  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions, and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#  
+#  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+#  
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+#  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+#  DAMAGE.
+#  @endparblock
+# @filedetails
+#
+#  I ran blend_risk twice -- once correlated and once uncorrelated.  I put the results in blend_risk_cor.txt & blend_risk_uncor.txt.  Then I stuck them
+#  together like this:
+#
+#    echo '     trial        hp             balance  Method' > blend_risk.txt
+#   
+#    grep -v trial blend_risk_cor.txt | sed 's/$/   Correlated/'   >> blend_risk.txt
+#   
+#    grep -v trial blend_risk_uncor.txt | sed 's/$/  Uncorrelated/' >> blend_risk.txt
+#
+#########################################################################################################################################################.H.E.##
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Set this to your favorite image viewer, or TRUE to attempt to find one automatically, or FALSE to not load images
