abe41b51c2788696478387854d126f891121315d
Mitch Richling <11151403+richmit@users.noreply.github.com>
2025-09-25 14:24:21 -0500

    Updated project root var. Updated docs

--- setup.mk
+++ setup.mk
@@ -1,11 +1,50 @@
-# Every makefile in this repository includes this one making this a
-# good place to set global variables.
+# -*- Mode:make; Coding:us-ascii-unix; fill-column:158 -*-
+#########################################################################################################################################################.H.S.##
+##
+# @file      setup.mk
+# @author    Mitch Richling http://www.mitchr.me/
+# @brief     Global makefile include.@EOL
+# @std       GNUmake
+# @see       https://github.com/richmit/FortranFinance/
+# @copyright 
+#  @parblock
+#  Copyright (c) 2025, Mitchell Jay Richling <http://www.mitchr.me/> All rights reserved.
+#  
+#  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#  
+#  1. Redistributions of source code must retain the above copyright notice, this list of conditions, and the following disclaimer.
+#  
+#  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions, and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#  
+#  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+#  
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+#  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+#  DAMAGE.
+#  @endparblock
+# @filedetails
+#
+#  Every makefile in this repository includes this one making this a good place to set global variables.
+#
+#########################################################################################################################################################.H.E.##
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------
-# The root directory for this project
-PROJECT_ROOT := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+## The root directory for this project
+FF_PROJ_ROOT := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------
-# Globally set FCOMP.
+## Automatically configure the compiler.  Valid values: flang, gfortran, ifx, lfortran, & nvfortran
 #FCOMP = ifx
 
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
+## Manually configure the compiler.
+#FCOMP  = NONE
+#AR     = --Command For Library Archive Tool--
+#FC     = --Command for Fortran compiler--
+#FFLAGS = --Compile flags/options for fortran compiler--
+#FSHFLG = --Shared library flags/options for fortran compiler--
